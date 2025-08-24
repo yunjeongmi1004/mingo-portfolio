63f1a98c2d6dd2f0499cd12bc6a28b157ad809fa
yunjeongmi1004 <love_j90@naver.com>
2025-08-24 22:06:11 +0900

    second commit

--- src/shared/layout/Footer/index.jsx
+++ src/shared/layout/Footer/index.jsx
@@ -1,8 +1,11 @@
+import footer1 from '../../../../public/footer_1.svg';
+import footer2 from '../../../../public/footer_2.svg';
+
 const Footer = () => {
   return (
     <footer className="flex justify-between items-center absolute bottom-[0] left-[0] w-full h-[22px] bg-[rgba(42,42,42,0.5)]">
-      <img src="/footer_1.svg" alt="footer" className="ml-[10px]"/>
-      <img src="/footer_2.svg" alt="footer" className="mr-[10px]"/>
+      <img src={footer1} alt="footer" className="ml-[10px]"/>
+      <img src={footer2} alt="footer" className="mr-[10px]"/>
     </footer>
   )
 }
--- src/shared/layout/Wrap/index.jsx
+++ src/shared/layout/Wrap/index.jsx
@@ -2,7 +2,7 @@
 
 const Wrap = ({ children }) => {
   return (
-    <div className="flex flex-col items-center justify-center h-screen bg-[#2e2f30] bg-cover bg-center">
+    <div className="flex flex-col items-center justify-center h-screen bg-[#515151] bg-cover bg-center">
       <div className="relative w-[90%] h-[90%] max-w-[1268px] max-h-[818px] rounded-[10px] overflow-hidden">
         {children}
       </div>
